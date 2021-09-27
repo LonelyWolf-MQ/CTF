ea004a2355b4f8824c56f7b50827260ce85ea98f
LonelyWolf-MQ <63113401+LonelyWolf-MQ@users.noreply.github.com>
2021-09-27 08:59:26 +0300

    Create solve.py

---
+++ cybertalent/forensics/Packet abomination/solve.py
@@ -0,0 +1,18 @@
+from scapy.all import *
+import codecs
+packet = rdpcap('Packet-abomination(1).pcap')
+pcts = []
+b = ""
+for p in packet:
+    if p.haslayer(DNS):
+        a = p.show(dump=True)
+        if 'load' in a:
+            data = p[DNS].load
+            pcts.append(codecs.encode(data,'hex').decode('utf-8'))
+        else:
+            continue    
+for i in sorted(pcts):
+	i = i[-2:]
+	b += i
+tohex = codecs.decode(b,"hex")
+print("flag is: "+ tohex.decode("utf-8"))
