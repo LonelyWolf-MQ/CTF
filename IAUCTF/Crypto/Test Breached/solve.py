0521fe1f41673b95f6fef938375e811c9d37e400
LonelyWolf-MQ <63113401+LonelyWolf-MQ@users.noreply.github.com>
2021-10-13 00:40:24 +0300

    Update solve.py

--- IAUCTF/Crypto/Test Breached/solve.py
+++ IAUCTF/Crypto/Test Breached/solve.py
@@ -1,1 +1,14 @@
 import os
+import hashlib
+import base64
+os.system("tshark -r test.pcapng --export-objects http,. > /dev/null 2>&1")
+with open('data.txt','r') as f:
+    lines = f.readline()
+decode = base64.b32decode(lines)
+with open('flag.jpeg' ,'wb') as f :
+	f.write(decode)
+with open("flag.jpeg", "rb") as f:
+	file_hash = hashlib.md5()
+	while chunk := f.read(8192):
+		file_hash.update(chunk)
+print("Flag is:  IAUflag{"+file_hash.hexdigest()+"}")
